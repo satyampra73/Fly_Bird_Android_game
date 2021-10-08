41f517ad7dbbe0898a0fcce00f8cd5affe2e84ad
Satyam Prajapati <satyam7355pra@gmail.com>
2021-10-08 16:12:30 +0530

    initial commit by Satyam

---
+++ android/src/com/first/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.first.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.first.game.Gameclass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Gameclass(), config);
+	}
+}
